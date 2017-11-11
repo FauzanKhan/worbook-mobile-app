30c9ade7b08779b724430a2a5e05946aab36e858
FauzanKhan <fauzankhan93@gmail.com>
2017-11-11 22:09:51 +0100

    Made Header beautiful

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,17 @@ import Icon from 'react-native-vector-icons/Ionicons';
 
 class Header extends Component {
   render() {
+    const { showBackButton } = this.props;
+
     return (
       <View style={styles.container}>
-        <Icon style={styles.backButton} name="ios-arrow-round-back" onPress={this.loginWithFacebook}></Icon>
-        <Text style={styles.text}>Dummy Title</Text>
+        { showBackButton &&
+            <Icon style={styles.backButton} name="ios-arrow-round-back" onPress={this.loginWithFacebook}></Icon>
+        }
+        <Text style={ showBackButton
+          ? { ...StyleSheet.flatten(styles.text) }
+          : { ...StyleSheet.flatten(styles.text), ...StyleSheet.flatten(styles.textCentered) }
+        }>WordBook</Text>
       </View>
     );
   }
@@ -17,8 +24,8 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#00aced',
     paddingHorizontal: 20,
-    paddingTop: 30,
-    paddingBottom: 10,
+    paddingTop: 35,
+    paddingBottom: 15,
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
@@ -28,8 +35,12 @@ const styles = StyleSheet.create({
     color: '#FFF',
     fontSize: 24,
     flexGrow: 1,
-    paddingLeft: 40,
     fontFamily: 'sans-serif-light',
+    textAlign: 'center',
+  },
+  logoText: {
+    textAlign: 'left',
+    paddingLeft: 40,
   },
   backButton: {
     color: '#FFF',
