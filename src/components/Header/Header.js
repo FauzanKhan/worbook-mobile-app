09f4af870dad9d9d208e0e3ac74709bbe3bcb5bb
FauzanKhan <fauzankhan93@gmail.com>
2017-11-12 01:40:54 +0100

    Made Header stateless

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import styles from './HeaderStyles';
 
-class Header extends Component {
-  render() {
-    const { title, goBack } = this.props;
-
-    return (
-      <View style={styles.container}>
-        { goBack &&
-          <TouchableOpacity style={styles.goBackLink} onPress={() => {console.log('called'); goBack()}}>
-            <Icon style={styles.goBackIcon} name="ios-arrow-round-back"/>
-          </TouchableOpacity>
-        }
-        <Text style={styles.title}>{title}</Text>
-      </View>
-    );
-  }
-}
+const Header = ({ title, goBack }) => (
+  <View style={styles.container}>
+    { goBack &&
+      <TouchableOpacity style={styles.goBackLink} onPress={goBack}>
+        <Icon style={styles.goBackIcon} name="ios-arrow-round-back" />
+      </TouchableOpacity>
+    }
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 export default Header;
