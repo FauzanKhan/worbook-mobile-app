679e3de5f6286032a9e3ff23c8a65f3eaf067ecb
FauzanKhan <fauzankhan93@gmail.com>
2017-11-12 15:01:45 +0100

    playing audio

--- src/components/Audio/Audio.js
+++ src/components/Audio/Audio.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import { View, TouchableNativeFeedback } from 'react-native';
-// import Sound from 'react-native-sound';
+import { Audio } from 'expo';
 
-// console.log('asdasd', Sound);
-
-class Audio extends Component {
+class AudioPlayer extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isPlaying: props.isPlaying,
     };
+
+    this.loadAudio = this.loadAudio.bind(this);
   }
 
   componentDidMount() {
+    this.loadAudio();
+  }
 
+  async loadAudio() {
+    console.log("SOURCE", this.props.source);
+    const a = this.state;
+    const soundObject = new Audio.Sound();
+    try {
+      await soundObject.loadAsync({ uri: this.props.source });
+      console.log("Loaded");
+      await soundObject.playAsync();
+    } catch (e) {
+      console.log("ERROR", e);
+    }
   }
 
   render() {
@@ -27,4 +40,4 @@ class Audio extends Component {
   }
 }
 
-export default Audio;
+export default AudioPlayer;
