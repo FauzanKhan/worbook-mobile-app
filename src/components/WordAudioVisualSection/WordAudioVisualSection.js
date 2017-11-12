60490f95fa3a957edb128a445d3c40d8b568ee8f
FauzanKhan <fauzankhan93@gmail.com>
2017-11-13 00:41:19 +0100

    Renamed Audio component to AudioPlayer

--- src/components/WordAudioVisualSection/WordAudioVisualSection.js
+++ src/components/WordAudioVisualSection/WordAudioVisualSection.js
@@ -3,7 +3,7 @@ import { Text, View, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { capitalize } from 'lodash';
 
-import Audio from '../Audio';
+import AudioPlayer from '../AudioPlayer';
 import styles from './WordAudioVisualSection.styles';
 
 const WordAudioVisualSection = ({ word, imageUrl, audioSrc }) => (
@@ -12,9 +12,9 @@ const WordAudioVisualSection = ({ word, imageUrl, audioSrc }) => (
     <View style={styles.overlayText}>
       <Text style={styles.title}>{capitalize(word)}</Text>
     </View>
-    <Audio source={audioSrc} style={styles.speakerTouchable}>
+    <AudioPlayer source={audioSrc} style={styles.speakerTouchable}>
       <Icon style={styles.icon} name="ios-play-outline" />
-    </Audio>
+    </AudioPlayer>
     <Image style={styles.image} source={{ uri: imageUrl }} />
   </View>
 );
