0e40f8ae615a8c715ee30521ee8c121724334adf
FauzanKhan <fauzankhan93@gmail.com>
2017-11-13 12:25:32 +0100

    Toggling state of practice button

--- src/components/PracticeButton/PracticeButton.js
+++ src/components/PracticeButton/PracticeButton.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableNativeFeedback } from 'react-native';
+// import { Audio } from 'expo';
 
 import styles from './PracticeButton.styles';
 
+const PLACEHOLDER_TEXT = 'PRACTICE';
+const RECORDING_TEXT = 'LISTENING...';
+
 class PracticeButton extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: PLACEHOLDER_TEXT,
+    };
+
+    this.startRecording = this.startRecording.bind(this);
+  }
+
+  startRecording() {
+    this.setState({
+      text: RECORDING_TEXT,
+    });
+  }
+
   render() {
     return (
-      <TouchableNativeFeedback>
+      <TouchableNativeFeedback onPress={this.startRecording}>
         <View style={styles.button}>
-          <Text style={styles.text}>PRACTICE</Text>
+          <Text style={styles.text}>{this.state.text}</Text>
         </View>
       </TouchableNativeFeedback>
     );
